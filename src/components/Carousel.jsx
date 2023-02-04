42e90c7c70e129daa5963a43491386435239a6b9
Guillermo Sanchez <guillesafran@gmail.com>
2023-02-04 08:41:57 -0300

    Add images and functionality to carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,28 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs'
 
 function Carousel () {
+  const USERS_FEEDBACK = [{ image: '/avatar-anisha.png', name: 'Anisha Li', body: 'Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.' },
+    { image: '/avatar-ali.png', name: 'Ali Bravo', body: 'We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.' },
+    { image: '/avatar-richard.png', name: 'Richard Watts', body: 'Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!' },
+    { image: '/avatar-shanai.png', name: 'Shanai Gough', body: 'Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.' }]
+
+  const [currentPosition, setCurrentPosition] = useState(0)
+  function nextSlide () {
+    const isLastSlide = currentPosition === USERS_FEEDBACK.length - 1
+    isLastSlide ? setCurrentPosition(() => 0) : setCurrentPosition((prev) => prev + 1)
+  }
+  function prevSlide () {
+    const isfirstSlide = currentPosition !== 0
+    isfirstSlide ? setCurrentPosition((prev) => prev - 1) : setCurrentPosition(() => USERS_FEEDBACK.length - 1)
+  }
   return (
-    <section className="p-7">
-    <h2 className="pr-8 pl-8 pb-2 text-center text-2xl font-extrabold text-gray-800">
-      What they&apos;ve said
-    </h2>
-    <figure className="flex flex-col place-items-center">
-      <img className="w-16" src="/avatar-ali.png" alt="Ali Bravo" />
-      <figcaption>
-        <b>Ali Bravo</b>
-      </figcaption>
-      <blockquote className="pt-2 pr-9 pl-9 text-center leading-loose text-xs text-gray-900 text-opacity-40">
-        &ldquo;We have been able to cancel so many other subscriptions since
-        using Manage. There is no more cross-channel confusion and
-        everyone is much more focused.&rdquo;
-      </blockquote>
-    </figure>
-    <div className="flex justify-center">
-      <button className="rounded-full bg-orange-600 p-2 pl-6 pr-6 text-sm font-semibold text-slate-200">
-        Get Started
-      </button>
-    </div>
-  </section>
+    <section className="p-7 relative group">
+
+      <BsChevronLeft onClick={prevSlide} className='absolute top-1/2 hidden h-6 w-6 group-hover:block'/>
+      <BsChevronRight onClick={nextSlide} className='hidden h-6 w-6 group-hover:block absolute top-1/2 right-7'/>
+
+      <h2 className="pr-8 pl-8 pb-2 text-center text-2xl font-extrabold text-gray-800">
+        What they&apos;ve said
+      </h2>
+      <figure className="flex flex-col place-items-center">
+        <img className="w-16" src={USERS_FEEDBACK[currentPosition].image} alt={USERS_FEEDBACK[currentPosition].name} />
+        <figcaption>
+          <b>{USERS_FEEDBACK[currentPosition].name}</b>
+        </figcaption>
+        <blockquote className="pt-2 pr-9 pl-9 text-center leading-loose text-xs text-gray-900 text-opacity-40">
+          &ldquo;{USERS_FEEDBACK[currentPosition].body}&rdquo;
+        </blockquote>
+      </figure>
+      <div className="flex justify-center">
+        <button className="rounded-full bg-orange-600 p-2 pl-6 pr-6 text-sm font-semibold text-slate-200">
+          Get Started
+        </button>
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import NavBackground from './NavBackground.jsx'
 import ButtonGetStarted from './ButtonGetStarted'
 
 function Navbar () {
@@ -19,6 +20,7 @@ function Navbar () {
           </header>
 
           <nav>
+            <NavBackground/>
             <div className='sm:hidden'> {/* Displays Hamburguer or close button in sm breakpoint */}
               <button
                 onClick={toggleHideNavModal}
@@ -54,6 +56,7 @@ function Navbar () {
         </header>
 
         <nav>
+          <NavBackground/>
           <div className='z-20 fixed inset-0 bg-black bg-opacity-25'>
             <div className='flex pt-6 pr-7 flex-row-reverse right-10'>
               <button onClick={toggleHideNavModal}>
@@ -78,39 +81,3 @@ function Navbar () {
 }
 
 export default Navbar
-
-/*
-<div className='relative flex w-screen justify-between p-7'>
-          <header>
-            <h1 className="invisible h-0 w-0">Manage Inc.</h1>
-            <img src="/logo.svg" alt="Logo Manage" />
-          </header>
-
-          <nav>
-            <div className={`${hideNavModal ? 'sm:hidden' : ''}`}>
-            <button
-            onClick={toggleHideNavModal}
-          >
-            <img src={`${hideNavModal ? '/icon-hamburger.svg' : '/icon-close.svg'}`} alt="Menu Icon" />
-          </button>
-        </div>
-
-        <div className={` ${hideNavModal ? 'hidden sm:block' : 'z-20 fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center'}`}>
-        <div className={`${hideNavModal ? 'sm:hidden' : ''}`}>
-          <button
-            onClick={toggleHideNavModal}
-          >
-            <img src={`${hideNavModal ? '/icon-hamburger.svg' : '/icon-close.svg'}`} alt="Menu Icon" />
-          </button>
-        </div>
-          <ul className={`flex ${hideNavModal ? '' : 'flex flex-col items-center gap-8 bg-white w-80 h-80'}` } >
-            {navLinks.map((link) => {
-              return (<li key={link}>{link}</li>)
-            })}
-            <li className='hidden md:block'><ButtonGetStarted/></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-
-*/
